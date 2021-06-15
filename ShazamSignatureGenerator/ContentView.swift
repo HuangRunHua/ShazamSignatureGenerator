fc79c539a3fd44bd2e081f5edc1ab4d866813e3e
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2021-06-15 16:12:57 +0800

    Initial Commit

---
+++ ShazamSignatureGenerator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShazamSignatureGenerator
+//
+//  Created by Runhua Huang on 2021/6/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShazamSignatureGenerator/ShazamSignatureGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamSignatureGeneratorApp.swift
+//  ShazamSignatureGenerator
+//
+//  Created by Runhua Huang on 2021/6/15.
+//
+
+import SwiftUI
+
+@main
+struct ShazamSignatureGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
